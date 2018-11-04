c5f7caa23670cc1ada2dbe92b3a87da2d1a33933
sfrieds3 <sfrieds3@gmail.com>
2018-11-04 16:35:48 -0600

    add recursice pascal traingle

---
+++ src/pascal.rb
@@ -0,0 +1,18 @@
+class Pascal
+  def initialize()
+  end
+
+  def pascal(row, column)
+    if row == column or row == 1
+      return 1
+    else
+      val = pascal(row-1, column) + pascal(row-1, column-1)
+      return val
+    end
+  end
+end
+
+if __FILE__ == $0
+  p = Pascal.new
+  p.pascal(6, 4)
+end
