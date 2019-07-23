81a142091a201776682be9b72a143d54dcac0cd7
sfrieds3 <sfrieds3@fastmail.com>
2019-07-23 17:49:22 -0500

    beginnings of a solution

--- src/leetcode/33/33_search_in_rotated_array.py
+++ src/leetcode/33/33_search_in_rotated_array.py
@@ -23,14 +23,42 @@ Output: -1
 
 
 class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+    def search(self, nums, target: int) -> int:
+        '''
+        known: target, len(list)
+        start at nums[target]:
+            if num > target:
+            if num < target:
+        '''
+
+        result: int = -1
+
+        list_mid: int = len(nums) // 2
+        test_val: int = nums[list_mid]
+        list_length: int = len(nums)
+
+        print(test_val)
+        print(list_length)
+        print(list_mid)
+
+        dist_to_target = list_length - target
+
+        if test_val < target:
+            print(dist_to_target)
+        else:
+            # test value > target
+            if dist_to_target == list_length:
+                if nums[list_mid + 1 + target] == target:
+                    result = list_mid + 1
+
+        return result
 
 
 class Test:
     def testSearch():
         solution: Solution = Solution()
-    assert(solution.search([4, 5, 6, 7, 0, 1, 2], 0) == 4)
-    assert(solution.search([4, 5, 6, 7, 0, 1, 2], 3) == -1)
+        assert(solution.search([4, 5, 6, 7, 0, 1, 2], 0) == 4)
+        assert(solution.search([4, 5, 6, 7, 0, 1, 2], 3) == -1)
 
 
 if __name__ == "__main__":
